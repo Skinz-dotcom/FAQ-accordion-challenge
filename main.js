91bd58b39dae4b0ad085e4d72758aa68d22ad16f
Skinz.dotcom <b.skinner1986@gmail.com>
2021-03-15 22:31:31 +0000

    Scrapped JS and started over. Found a new way to accomplish the task without bugs. Had to adjust CSS to support the new JS logic.

--- main.js
+++ main.js
@@ -1,34 +1,25 @@
-// logic for accordion
+// ! logic for accordion
 
-let acc = document.querySelectorAll("button");
-let panel = document.querySelectorAll(".accordian_panel");
-let i;
+// select all the buttons as a Node list and set as a Variable.
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    document.querySelectorAll(".accordian_panel.show").forEach((element) => {
-      if (element !== panel) {
-        element.classList.remove("show");
-        element.classList.add("hidden");
-      }
-    });
+let accordionBtn = document.querySelectorAll(".accordian_btn");
 
-    document.querySelectorAll(".accordian_btn").forEach((element) => {
-      if (element !== acc) {
-        element.classList.remove("active");
-        //   element.classList.add("hidden");
-      }
-    });
+// ? add a for each loop that applied an event listener to the accordian buttons.
+// The click triggers an if/else statement.
+// ! if the parentNode (accordian_item div) has a class of active, then toggle the class active. This allows us to click the same question to open and close.
+// else run another forEach loop but this time over the accordian_item div and remove the active class. This closes all the accordian_panels if any are open.
+// ? then add the class active to the accordian_item clicked to open the panel.
+// This lets us open only one answer planel at a time, resetting anything previously opened, but also allows us to open and close the same answer panel by clicking it again.
 
-    this.classList.toggle("active");
-    this.nextElementSibling.classList.toggle("hidden");
-    this.nextElementSibling.classList.toggle("show");
-
-    // let panel = this.nextElementSibling;
-    // if (panel.style.display === "block") {
-    //   panel.style.display = "none";
-    // } else {
-    //   panel.style.display = "block";
-    // }
+accordionBtn.forEach((question) => {
+  question.addEventListener("click", () => {
+    if (question.parentNode.classList.contains("active")) {
+      question.parentNode.classList.toggle("active");
+    } else {
+      accordionBtn.forEach((question) =>
+        question.parentNode.classList.remove("active")
+      );
+      question.parentNode.classList.add("active");
+    }
   });
-}
+});
